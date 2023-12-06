74cf20dc2f8f9d22f5c94b934850efec45be98b1
Brian Baliach <brianbaliach@Brians-MacBook-Pro.local>
2023-12-06 15:13:24 +0300

    added queries, mutators, input and resolvers

--- src/index.ts
+++ src/index.ts
@@ -10,11 +10,63 @@ const resolvers = {
     games() {
       return db.games;
     },
+    game(_, args) {
+      return db.games.find((review) => args.id === review.id);
+    },
     reviews() {
       return db.reviews;
     },
+    review(_, args) {
+      return db.reviews.find((review) => args.id === review.id);
+    },
     authors() {
       return db.authors;
+    },
+    author(_, args) {
+      return db.authors.find((review) => args.id === review.id);
+    },
+  },
+  Game: {
+    reviews(parent) {
+      return db.reviews.filter((review) => review.game_id === parent.id);
+    },
+  },
+  Author: {
+    reviews(parent) {
+      return db.reviews.filter((review) => review.author_id === parent.id);
+    },
+  },
+  Review: {
+    author(parent) {
+      return db.authors.find(a => a.id === parent.author_id);
+    },
+    game(parent) {
+      return db.games.find(a => a.id === parent.game_id);
+    },
+  },
+  Mutation: {
+    deleteGame(_, args) {
+      db.games = db.games.filter(g => g.id !== args.id);
+      return db.games;
+    },
+    addGame(_, args) {
+      let game = {
+        ...args.game,
+        id: Math.floor(Math.random() * 10000).toString(),
+      };
+      db.games.push(game);
+      return game;
+    },
+    updateGame(_, args) {
+      db.games = db.games.map(g => {
+        if (g.id === args.game.id) {
+          return {
+            ...g, ...args.game,
+          };
+        }
+        return g;
+      });
+      return args.game;
     },
   },
 };
@@ -30,4 +82,4 @@ const {url} = await startStandaloneServer(server, {
   },
 });
 
-console.log("Server ready at port ", port);
+console.log(`Server ready at ${url}`);
--- src/schema.ts
+++ src/schema.ts
@@ -1,25 +1,49 @@
 const typeDefs = `#graphql
 type Game {
     id: ID!,
-    title: String,
-    platform: [String]
+    title: String!,
+    platform: [String]!
+    reviews: [Review!]
 }
 type Review {
     id: ID!
     rating: Int!
     content: String!
+    author: Author!
+    game: Game!
 }
 
 type Author {
     id: ID!
     name: String!
     verified: Boolean!
+    reviews: [Review]!
 }
 
 type Query {
     reviews: [Review]
+    review(id: ID!): Review
     games: [Game]
+    game(id: ID!): Game
     authors: [Author]
+    author(id: ID!): Author
+}
+
+type Mutation {
+    addGame(game: AddGameInput!): Game
+    updateGame(game: EditGameInput!): Game
+    deleteGame(id: ID!): [Game]
+}
+
+input AddGameInput {
+    title: String!
+    platform: [String!]!
+}
+
+input EditGameInput {
+    id: ID!
+    title: String
+    platform: [String!]
 }
 `;
 
